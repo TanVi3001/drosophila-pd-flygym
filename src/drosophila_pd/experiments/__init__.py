91bc44c25dd25ea1ac409001efafc74dda018ce8
Le Tan Vi <papylovevip1@gmail.com>
2026-08-11 16:14:19 +0700

    feat: add Milestone C healthy locomotion baseline

--- src/drosophila_pd/experiments/__init__.py
+++ src/drosophila_pd/experiments/__init__.py
@@ -1,1 +1,17 @@
+"""Experiment orchestration for Drosophila PD FlyGym workflows."""
 
+from .healthy_baseline import (
+    DEFAULT_HEALTHY_BASELINE_CONFIG,
+    HealthyBaselineConfig,
+    build_healthy_baseline_unavailable_report,
+    load_healthy_baseline_config,
+    run_healthy_baseline,
+)
+
+__all__ = [
+    "DEFAULT_HEALTHY_BASELINE_CONFIG",
+    "HealthyBaselineConfig",
+    "build_healthy_baseline_unavailable_report",
+    "load_healthy_baseline_config",
+    "run_healthy_baseline",
+]
--- src/drosophila_pd/metrics/__init__.py
+++ src/drosophila_pd/metrics/__init__.py
@@ -1,1 +1,8 @@
+"""Metrics for Drosophila PD FlyGym simulations."""
 
+from .locomotion import check_locomotion_pass_criteria, compute_locomotion_metrics
+
+__all__ = [
+    "check_locomotion_pass_criteria",
+    "compute_locomotion_metrics",
+]
